4c3f126ce0e6726d0db76e8b3c234c26d7351b51
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2025-01-16 12:10:00 +0400

    Correct frontend folder name

--- node-app/server.js
+++ node-app/server.js
@@ -3,7 +3,7 @@ const app = express()
 //const port = 4000
 const EXPRESS_PORT = process.env.EXPRESS_PORT || 4000
 
-app.use(express.static('frontend'))
+app.use(express.static('../front'))
 
 app.get('/info', (req, res) => {
     res.status(200).json({info: 'preset text'})
